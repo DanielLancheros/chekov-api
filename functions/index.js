2d2e72acbb751810434eb0c5cdb3f2817e2f75e7
Daniel Lancheros <daniellancheros@Daniels-MacBook-Air.local>
2023-07-05 11:01:25 -0400

    WIP

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,14 @@
 import { onRequest } from "firebase-functions/v1/https";
 import express from "express";
 import cors from "cors";
+import { getTasks, addTask } from "./src/tasks.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 // routes: 
-app.get('/tasks', getTasks);
+app.get('/tasks:uid', getTasks);
 app.post('/tasks', addTask);
 
 export const api = onRequest(app); // exports cloud function
--- functions/src/dbConnect.js
+++ functions/src/dbConnect.js
@@ -2,3 +2,10 @@ import { initializeApp, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import creds from "..creds.js";
 
+// this connects us to our firebase project
+initializeApp({
+    credential: cert(creds)
+});
+
+export default getFirestore() // this connects to our DB
+
